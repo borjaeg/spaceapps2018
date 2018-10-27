4d1e3c36c32878f252b423172d3648e2d080a35a
Borja Espejo García <borjaeg@MacBook-Pro-de-Borja.local>
2018-10-27 18:55:10 +0200

    app dockerized

--- services/fires_service.py
+++ services/fires_service.py
@@ -1,11 +1,6 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-	return render_template('index.html')
-
 
 @app.route('/fires')
 def get_data():
